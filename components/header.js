fc694d3df5293f08ee0bf1385b1a16059c96b053
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-02 11:59:04 -0500

    Added styling and SVG for header component

--- components/header.js
+++ components/header.js
@@ -1,19 +1,25 @@
 import react from "react";
 import Image from "next/image";
+import Link from 'next/link';
 
 const Header = () => {
+
     return (
-        <header>
-            <Image 
-                src="/web-link.svg"
-                alt="Chain Logo"
-                height={100}
-                width={100}
-            />
-            <nav>
-                <ul>
-                    <li>Balance</li>
-                    <li>NFT</li>
+        <header className="flex items-center justify-around p-5">
+            <Link href="/">
+                <Image 
+                    src="/web-link.svg"
+                    alt="Block Explorer Logo"
+                    className="cursor-pointer"
+                    height={75}
+                    width={75}
+                />
+            </Link>
+            <nav>                
+                <ul className="flex list-none gap-5">
+                    <Link href="/"><li className="text-lg cursor-pointer">Home</li></Link>
+                    <Link href="/balance"><li className="text-lg cursor-pointer">Balance</li></Link>
+                    <Link href="/nft"><li className="text-lg cursor-pointer">NFT</li></Link>
                 </ul>
             </nav>
         </header>
