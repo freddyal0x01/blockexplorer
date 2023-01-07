52e7a27cdaf9dc37693a3fa82aa58dedf96001aa
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-07 15:40:31 -0500

    Added a background color to header

--- components/header.js
+++ components/header.js
@@ -5,21 +5,21 @@ import Link from 'next/link';
 const Header = () => {
 
     return (
-        <header className="flex items-center justify-around p-5">
+        <header className="flex items-center justify-around p-5 bg-neutral-200">
             <Link href="/">
                 <Image 
                     src="/web-link.svg"
                     alt="Block Explorer Logo"
                     className="cursor-pointer"
-                    height={75}
-                    width={75}
+                    height={60}
+                    width={60}
                 />
             </Link>
             <nav>                
                 <ul className="flex list-none gap-5">
-                    <Link href="/"><li className="text-lg cursor-pointer">Home</li></Link>
-                    <Link href="/balance"><li className="text-lg cursor-pointer">Balance</li></Link>
-                    <Link href="/nft"><li className="text-lg cursor-pointer">NFT</li></Link>
+                    <Link href="/"><li className="text-lg cursor-pointer font-semibold">Home</li></Link>
+                    <Link href="/balance"><li className="text-lg cursor-pointer font-semibold">Balance</li></Link>
+                    <Link href="/nft"><li className="text-lg cursor-pointer font-semibold">NFT</li></Link>
                 </ul>
             </nav>
         </header>
