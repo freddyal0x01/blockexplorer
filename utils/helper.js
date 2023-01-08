c10e67ca7d2cbddff746e2a2a97e67f2a86512f0
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-08 18:01:34 -0500

    Added 2 helper functions

---
+++ utils/helper.js
@@ -0,0 +1,10 @@
+import { Utils } from 'alchemy-sdk';
+
+
+export const formatAddress = (address) => {
+    return `${address.slice(0,5)}...${address.slice(--5)}`
+}
+
+export const formatEther = (wei) => {
+    return Utils.formatEther(wei);
+}
