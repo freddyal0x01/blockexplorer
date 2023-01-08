0672f04cac362e7ad5c72eea1e517e4d98bc1437
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-08 18:03:00 -0500

    Added comments for helper functions

--- utils/helper.js
+++ utils/helper.js
@@ -1,10 +1,11 @@
 import { Utils } from 'alchemy-sdk';
 
-
+// Formatted eth address for UI purposes
 export const formatAddress = (address) => {
     return `${address.slice(0,5)}...${address.slice(--5)}`
 }
 
+// Convert Wei over to Eth
 export const formatEther = (wei) => {
     return Utils.formatEther(wei);
 }
