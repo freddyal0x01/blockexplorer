e3229a9f33fd372c95f7450e4fb74047870949ac
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-08 17:57:17 -0500

    Installed Alchemy and added a helper function

--- utils/services.js
+++ utils/services.js
@@ -2,9 +2,24 @@ import { Network, Alchemy } from 'alchemy-sdk';
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-  apiKey: REACT_APP_ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
+  apiKey: "lGcVaxhcnSrKzVIv5t1ZlQRxPiTwK4bk", // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 
+// console.log(process.env.ALCHEMY_API_KEY)
+// console.log(settings)
+
 const alchemy = new Alchemy(settings);
 
+
+
+
+
+const getAccountEthBalance = async (address) => {
+  const balance = await alchemy.core.getBalance(address, "latest");
+  return balance;
+}
+
+const balance = await getAccountEthBalance("0xa40199aebe80d41b7d06d06fd8982c3ea5f70514");
+
+console.log(balance.toString());
