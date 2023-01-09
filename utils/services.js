718283e960d6e34b1e73cada5f11ab019a4a2375
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-09 00:27:52 -0500

    Added function where I get all of the transactions from an ETH address

--- utils/services.js
+++ utils/services.js
@@ -11,13 +11,26 @@ const settings = {
 
 const alchemy = new Alchemy(settings);
 
+// Obtain all of the txs from an ETH address
+export const getAccountTxs = async (address) => {
+  txHistory = {
+    fromBlock: "0x0",
+    fromAddress: address,
+    excludeZeroValue: true,
+    category: ["external", "internal", "erc20", "erc721", "erc1155"],
+  }
+  const txs = await alchemy.core.getAssetTransfers(txHistory);
+  return txs;
+}
 
-
-
-// Get the balance in wei from an eth address. 
+// Get the balance in wei from an eth address
 export const getAccountEthBalance = async (address) => {
   const balance = await alchemy.core.getBalance(address, "latest");
   return balance;
 }
 
-
+// Get the NFT metadata based off the contract address and token ID
+export const getNftMetadata = async(contractAddress, tokenId) => {
+  const nftMetadata = await alchemy.nft.getNftMetadata(contractAddress, tokenId);
+  return nftMetadata;
+}
