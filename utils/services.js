e8d4d16072f446b2c7595ffe09acfc99305a59a4
freddyal <freddyal0x01@gmail.com>
2023-01-09 23:53:55 -0500

    use env variable to obfuscate API key

--- utils/services.js
+++ utils/services.js
@@ -1,13 +1,13 @@
 import { Network, Alchemy } from 'alchemy-sdk';
 
+import { config } from 'dotenv';
+config()
+
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-  apiKey: "lGcVaxhcnSrKzVIv5t1ZlQRxPiTwK4bk", // Replace with your Alchemy API Key.
-  network: Network.ETH_MAINNET, // Replace with your network.
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
+  network: Network.ETH_GOERLI, // Replace with your network.
 };
-
-// console.log(process.env.ALCHEMY_API_KEY)
-// console.log(settings)
 
 const alchemy = new Alchemy(settings);
 
