1e2952d6f68109c0f87b8b534089720d49e51b1e
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-16 11:44:13 -0500

    Changed ENV variable name

--- utils/services.js
+++ utils/services.js
@@ -2,7 +2,7 @@ import { Network, Alchemy } from 'alchemy-sdk';
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 export const settings = {
-  apiKey: process.env.API_KEY, // Replace with your Alchemy API Key.
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 
