88c83e663df773cb73e91eec0100a09462f69dc0
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-16 21:43:39 -0500

    Created initial layout for blocks & txs components

--- components/blocks.js
+++ components/blocks.js
@@ -1,10 +1,19 @@
-
+import { alchemy } from "../utils/services";
 
 const Blocks = () => {
 
+    const getBlock = async () => {
+        const blockNumber = await alchemy.core.getBlockNumber();
+        console.log(blockNumber)
+
+        const txs = await alchemy.core.getBlockWithTransactions(blockNumber);
+        console.log(txs)
+    }
+
     return (
-        <section className="shadow-2xl w-4/5 bg-stone-50	">
+        <section className="self-center shadow-2xl my-2 sm:mx-2 w-4/5 bg-white">
             <h2 className="text-center text-2xl text-gray-900 font-semibold">Blocks</h2>
+            {getBlock()}
         </section>
     )
 }
--- components/transactions.js
+++ components/transactions.js
@@ -3,7 +3,7 @@
 const Transactions = () => {
 
     return (
-        <section className="shadow-2xl w-4/5 bg-white">
+        <section className="self-center shadow-2xl my-1 sm:mx-2 w-4/5 bg-white">
             <h2 className="text-center text-2xl text-gray-900 font-semibold shadow-2xl">Transactions</h2>
         </section>
     )
