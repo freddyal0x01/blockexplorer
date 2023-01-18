62231e71e88be203bfade1b8bfdedecb1fce7cf1
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-17 23:35:53 -0500

    Added Block Logic

--- components/blocks.js
+++ components/blocks.js
@@ -1,20 +1,30 @@
-import { alchemy } from "../utils/services";
+import { useEffect, useState } from "react";
+import { formatAddress } from "../utils/helper";
+import { getLastTenBlocks } from "../utils/services";
 
 const Blocks = () => {
+    const [blocks, setBlocks] = useState([]);
 
-    const getBlock = async () => {
-        const blockNumber = await alchemy.core.getBlockNumber();
-        console.log(blockNumber)
-
-        const txs = await alchemy.core.getBlockWithTransactions(blockNumber);
-        console.log(txs)
-    }
+    useEffect(() => {
+        (async() => {
+            const _blocks = await getLastTenBlocks();
+            setBlocks(_blocks);
+        })();
+    })
 
     return (
-        <section className="self-center shadow-2xl my-2 sm:mx-2 w-4/5 bg-white">
-            <h2 className="text-center text-2xl text-gray-900 font-semibold">Blocks</h2>
-            {getBlock()}
-        </section>
+        <article className="self-center shadow-2xl p-4 my-2 sm:mx-2 w-4/5 bg-white">
+            <h2 className="text-center text-2xl text-gray-900 font-semibold mb-2">Blocks</h2>
+            {
+                blocks.map(({miner, number, transactions}) => (
+                    <section className="text-center grid grid-cols-3 p-2 bg-slate-50 border-slate-600 border-2 m-2">
+                        <p>Block #: {number}</p>
+                        <p># of Txs: <span className="text-blue-600">{transactions.length}</span></p>
+                        <p>Validator Address: <span className="text-blue-600 break-all">{miner}</span></p>
+                    </section>
+                )) 
+            } 
+        </article>
     )
 }
 
