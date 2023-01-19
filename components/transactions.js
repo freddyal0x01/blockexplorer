ee3c8e1e20729de9f1635f25c2859d53a5ca2b6c
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-18 22:31:43 -0500

    Added logic for the tx component on the home page

--- components/transactions.js
+++ components/transactions.js
@@ -1,11 +1,39 @@
-
+import { useEffect, useState } from "react";
+import { formatAddress, formatEther } from "../utils/helper";
+import { getLastTenTxs } from "../utils/services";
 
 const Transactions = () => {
 
+    const [txs, setTxs] = useState([]);
+
+    useEffect(() => {
+        (async() => {
+            const _txs = await getLastTenTxs();
+            setTxs(_txs);
+        })();
+    }, [txs])
+    // async function getInfo() {
+    //     const block = await getLastTenTxs()
+    //     console.log(block)
+    //     // console.log(tx["hash"])
+    // }
+    
+    // getInfo()
+
     return (
-        <section className="self-center shadow-2xl p-4 my-2 sm:mx-2 w-4/5 bg-white">
-            <h2 className="text-center text-2xl text-gray-900 font-semibold">Transactions</h2>
-        </section>
+        <article className="self-center shadow-2xl p-4 my-2 sm:mx-2 w-4/5 bg-white">
+            <h2 className="text-center text-2xl text-gray-900 font-semibold mb-2">Transactions</h2>
+            {
+                txs.map(({hash, from, to, value}, index) => (
+                    <section key={index} className="text-center grid grid-cols-4 p-2 bg-slate-50 border-slate-600 border-2 m-2">
+                        <p>Tx Hash: <span className="text-blue-600 break-all">{formatAddress(hash)}</span></p>
+                        <p>From: <span className="text-blue-600 break-all">{formatAddress(from)}</span></p>
+                        <p>To: <span className="text-blue-600 break-all">{formatAddress(to)}</span></p>
+                        <p>Amount: <span className="text-blue-600 break-all">{formatEther(value)} ETH</span></p>
+                    </section>
+                )) 
+            }
+        </article>
     )
 }
 
