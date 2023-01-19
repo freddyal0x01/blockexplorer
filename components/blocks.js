c36c0c43b31190da50fa6c80aaf5ac6fa0c204af
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-18 22:31:22 -0500

    Formatted miner address & added a key to the prop

--- components/blocks.js
+++ components/blocks.js
@@ -10,17 +10,17 @@ const Blocks = () => {
             const _blocks = await getLastTenBlocks();
             setBlocks(_blocks);
         })();
-    })
+    }, [blocks])
 
     return (
         <article className="self-center shadow-2xl p-4 my-2 sm:mx-2 w-4/5 bg-white">
             <h2 className="text-center text-2xl text-gray-900 font-semibold mb-2">Blocks</h2>
             {
-                blocks.map(({miner, number, transactions}) => (
-                    <section className="text-center grid grid-cols-3 p-2 bg-slate-50 border-slate-600 border-2 m-2">
+                blocks.map(({miner, number, transactions}, index) => (
+                    <section key={index} className="text-center grid grid-cols-3 p-2 bg-slate-50 border-slate-600 border-2 m-2">
                         <p>Block #: {number}</p>
                         <p># of Txs: <span className="text-blue-600">{transactions.length}</span></p>
-                        <p>Validator Address: <span className="text-blue-600 break-all">{miner}</span></p>
+                        <p>Validator Address: <span className="text-blue-600 break-all">{formatAddress(miner)}</span></p>
                     </section>
                 )) 
             } 
