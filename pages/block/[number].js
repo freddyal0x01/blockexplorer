213b164d67554e142fb0af32d68ef93383f9161f
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-18 23:44:49 -0500

    Got the skeleton for Block component going

--- pages/block/[number].js
+++ pages/block/[number].js
@@ -1,25 +1,34 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Header from '../../components/header';
+import { getIndividualBlock } from '../../utils/services';
 
 
 const Block = () => {
-    const router = useRouter()
-    const { number } = router.query
-    console.log(router.query);
+    const router = useRouter();
+    
+    const { number } = router.query;
+    
     console.log(number);
+
+    // getIndividualBlock
 
     return (
         <>
             <Head>
-                <title>Block {}</title>
-                <meta name="description" content={`Details about block # `} />
+                <title>Block {number}</title>
+                <meta name="description" content={`Details about block # ${number}`} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <Header />
-            
+
+            <main className='flex flex-col justify-center items-center text-center'>
+                <article className='gap-4 my-8 w-4/5 shadow-2xl p-4 bg-white'>
+                    <h1 className='text-3xl font-bold mt-12'>Block Details</h1>
+                </article>
+            </main>
         </>
     )
 }
