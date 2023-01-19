f75875f897e14541e50361652829beeae453e519
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-18 23:26:44 -0500

    Added dynamic endpoint for Block Page

---
+++ pages/block/[number].js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Header from '../../components/header';
+
+
+const Block = () => {
+    const router = useRouter()
+    const { number } = router.query
+    console.log(router.query);
+    console.log(number);
+
+    return (
+        <>
+            <Head>
+                <title>Block {}</title>
+                <meta name="description" content={`Details about block # `} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Header />
+            
+        </>
+    )
+}
+
+export default Block;
