66e1b3551fce6fb68fe3d691a53fdaea0c3fe1f2
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-20 01:49:19 -0500

    Removed exporting the settings function

--- utils/services.js
+++ utils/services.js
@@ -1,7 +1,7 @@
 import { Network, Alchemy } from 'alchemy-sdk';
 
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
-export const settings = {
+const settings = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
